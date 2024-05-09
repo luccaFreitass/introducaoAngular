c37e599d889cd7fca9c2fd0adf667021417f8d60
luccaFreitass <luccadefreitasfaria@hotmail.com>
2024-05-08 22:21:01 -0300

    add two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'introducao-angular';
 
-  isMouseOver: boolean = false;
-  valorAtual: string = '';
-  valorSalvo: string = '';
-  desabilitado: boolean = false;
-
-  botaoClicado() {
-    alert('Botão clicado!');
-    this.desabilitado = !this.desabilitado;
-  }
-
-  onKeyUp(event: KeyboardEvent) {
-    this.valorAtual += event.key;
-  }
-
-  salvarValor(event: any) {
-    this.valorSalvo = event;
-  }
-
-  onMouseOverOut() {
-    this.isMouseOver = !this.isMouseOver;
-  }
+  nome: String = "Teste";
 }
